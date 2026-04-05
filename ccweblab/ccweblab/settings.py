54f62f5443a383aee0168130a0042dbf6824151f
Sebascarv1 <my.email@example.com>
2026-04-06 00:54:26 +0200

    fix

--- ccweblab/ccweblab/settings.py
+++ ccweblab/ccweblab/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
